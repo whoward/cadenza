7a802198b62e240b19b8bfb84433d11609be1952
William Howard <whoward.tke@gmail.com>
2012-02-14 07:56:09 -0500

    large update to the syntax section of the manual

--- doc/src/context.rb
+++ doc/src/context.rb
@@ -7,13 +7,28 @@ push({
   'now' => Time.now,
   'nil' => nil,
   'empty_list' => [],
+  'name' => 'John Doe',
+  'something_true' => true,
+  'something_false' => false,
 })
 
+define_statement :example do |context, template|
+  result = "<pre class='source'>"
+  result << context.evaluate_statement("load", [template])
+  result << "</pre>"
+
+  result << "<pre class='rendered'>"
+  result << context.evaluate_statement("render", [template])
+  result << "</pre>"
+
+  result
+end
+
 define_block :code_example do |context, nodes, parameters|
-   result = "<pre>"
-   nodes.each do |node|
-      result << CGI.escapeHTML(Cadenza::TextRenderer.render(node, context))
-   end
-   result << "</pre>"
-   result
+  result = "<pre>"
+  nodes.each do |node|
+    result << CGI.escapeHTML(Cadenza::TextRenderer.render(node, context))
+  end
+  result << "</pre>"
+  result
 end
