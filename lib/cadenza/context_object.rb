2dd3b9c6bb51944b0c58d2b1ea5fcabe81011851
William Howard <whoward.tke@gmail.com>
2012-10-21 00:38:58 -0400

    change Cadenza::ContextObject to subclass Object as BasicObject is only available in Ruby 1.9.x

--- lib/cadenza/context_object.rb
+++ lib/cadenza/context_object.rb
@@ -1,7 +1,10 @@
 
 module Cadenza
-   class ContextObject < BasicObject
+   class ContextObject < Object # intentionally declared, since we cannot 
+                                # subclass BasicObject in 1.8.7
+
       # this class is intentionally left blank as a placeholder, see 
       # context_object_spec.rb under spec/ for details.
+      
    end
 end
