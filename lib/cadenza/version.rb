407349bf234f1937df18198b7a13567c924cff1e
William Howard <whoward.tke@gmail.com>
2012-10-31 04:36:33 -0400

    bump version back to 0.8.0

--- lib/cadenza/version.rb
+++ lib/cadenza/version.rb
@@ -2,8 +2,8 @@
 module Cadenza
   module Version
      MAJOR = 0
-     MINOR = 7
-     PATCH = 2
+     MINOR = 8
+     PATCH = 0
      BUILD = nil
     
      STRING = [MAJOR, MINOR, PATCH, BUILD].compact.join('.')
