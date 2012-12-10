0a84ba0dbd7d5ca6be32fbd28e0a553febb0f212
William Howard <whoward.tke@gmail.com>
2012-12-10 00:03:32 -0500

    document BlockHierarchy

--- lib/cadenza/block_hierarchy.rb
+++ lib/cadenza/block_hierarchy.rb
@@ -1,19 +1,30 @@
 
 module Cadenza
+   # This class is used to help implement the "super" magic variable which is
+   # available when rendering blocks using the {TextRenderer}.  It is essentially
+   # a glorified hash table with some special rules for merging.
+   #
+   # Please treat this class as private to Cadenza, it is not meant to be used
+   # outside of this gem.
    class BlockHierarchy
 
+      # creates a new {BlockHierarchy} with the initial block hash
+      # @param [Hash] data the initial data to merge into the names hash
       def initialize(data=nil)
          @names = Hash.new
 
          merge(data) if data
       end
 
+      # @return [Array] the inheritance chain for the given block name
       def fetch(block_name)
          @names[block_name.to_s] || []
       end
 
       alias :[] :fetch
 
+      # appends the given block to the inheritance chain of it's name
+      # @param [BlockNode] block
       def push(block)
          @names[block.name.to_s] ||= []
          @names[block.name.to_s] << block
@@ -21,6 +32,9 @@ module Cadenza
 
       alias :<< :push
 
+      # merges the given hash of blocks (name -> block) onto the end of each
+      # inheritance chain
+      # @param [Hash] hash
       def merge(hash)
          hash.each {|k,v| self << v }
       end
